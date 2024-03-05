5b6bc9c40550eb80bec53ccb90b6c9053166b446
simone-nolberto <simone.nolberto@outlook.com>
2024-03-05 14:58:45 +0100

    add
    snack 3

---
+++ snack3/main3.js
@@ -0,0 +1,8 @@
+const animals = [
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+];
+
+const mammals = animals.filter(member => member.classe === 'mammiferi');
+console.log(mammals);
