89bc9785914baa31d27f859fb202ba9f73181658
simone-nolberto <simone.nolberto@outlook.com>
2024-03-06 12:23:10 +0100

    add
    snack 2-b5

---
+++ blocco5/snack2/main.js
@@ -0,0 +1,94 @@
+console.log('it workss!');
+
+// Crea 10 oggetti che rappresentano una zucchina.
+
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+const zuchinis = [
+    {
+        nome: 'zucchina romanesca',
+        peso: 90,
+        lunghezza: 15,
+    },
+    {
+        nome: 'zucchina trombetta',
+        peso: 60,
+        lunghezza: 10,
+    },
+    {
+        nome: 'zucchina costata',
+        peso: 110,
+        lunghezza: 20,
+    },
+    {
+        nome: 'zucchina crookneck',
+        peso: 50,
+        lunghezza: 10,
+    },
+    {
+        nome: 'zucchina rampicante',
+        peso: 130,
+        lunghezza: 30,
+    },
+    {
+        nome: 'zucchina italiana',
+        peso: 120,
+        lunghezza: 18,
+    },
+    {
+        nome: 'zucchina gialla',
+        peso: 125,
+        lunghezza: 20,
+    },
+    {
+        nome: 'zucchina siciliana',
+        peso: 90,
+        lunghezza: 15,
+    },
+    {
+        nome: 'zucchina gorda',
+        peso: 130,
+        lunghezza: 21,
+    },
+    {
+        nome: 'zucchina bianca',
+        peso: 112,
+        lunghezza: 16,
+    },
+];
+
+console.log(zuchinis);
+
+let short = zuchinis.filter((zuchini) => {
+    if (zuchini.lunghezza < 15) {
+        return true;
+    }
+    return false;
+})
+console.log(short);
+
+let shortWeight = 0;
+short.forEach(element => {
+    shortWeight += element.peso
+})
+console.log(`${shortWeight}g`);
+
+
+
+let long = zuchinis.filter((zuchini) => {
+    if (zuchini.lunghezza >= 15) {
+        return true;
+    }
+    return false;
+})
+console.log(long);
+
+let longWeight = 0;
+long.forEach(element => {
+    longWeight += element.peso
+})
+console.log(`${longWeight}g`);
+
+
